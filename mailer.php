95b483009f85bf5ec9d224d67bd61e9d2aa3ae82
Gadgetboy <Gadgetboy@users.noreply.github.com>
2019-09-03 21:14:36 +0100

    Changes to Website opening times, updated copywrite 2019
    Requested by Carl 26/08/2019

--- mailer.php
+++ mailer.php
@@ -1,6 +1,6 @@
 <?php
 /* Set e-mail recipient */
-$myemail = "haighscarpets@gmail.com";
+$myemail = "haighscarpets@outlook.com";
 
 /* Check all form inputs using check_input function */
 $name = check_input($_POST['inputName'], "Your Name");
